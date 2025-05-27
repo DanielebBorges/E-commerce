09cd80b5674ef92dde2896d4c99939d09b518a93
Daniele Borges <143644899+DanielebBorges@users.noreply.github.com>
2025-05-27 17:25:19 -0300

    fonte atualizada

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
     <footer class="fixed bottom-0 left-0 w-full bg-[#c115ef] text-white py-4 text-center shadow">
-        <p>© 2025 <strong>Bardaniê</strong> - Todos os direitos reservados</p>
+        <p>© 2025 <span class="font-display">Bardaniê</span> - Todos os direitos reservados</p>
     </footer>
 </template>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="bg-[#c115ef] text-white py-4 px-8 flex justify-between items-center">
-        <h1 class="text-lg front-bold">
+        <h1 class="text-lg front-bold font-display">
             Bardaniê
         </h1>
         <ul class="flex gap-6">
@@ -8,4 +8,15 @@
 
         </ul>
     </nav>
+ 
+
 </template>
+<style>
+:root {
+  --font-display: 'Abril Fatface', cursive;
+}
+
+.font-display {
+  font-family: var(--font-display);
+}
+</style>
